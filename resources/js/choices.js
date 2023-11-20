2d6ff15ace65ef487324ba493a8f43b7f9850796
Etienne CHIAPPINO <chiappinoe@gmail.com>
2023-11-20 10:48:09 +0100

    Execute Choices.js only when need it

--- resources/js/choices.js
+++ resources/js/choices.js
@@ -1,8 +1,10 @@
 import Choices from "choices.js";
 import "choices.js/public/assets/styles/choices.min.css";
 
-const multipleSelect = document.querySelector(".js-choices");
-const choices = new Choices(multipleSelect, {
-    removeItemButton: true,
-    maxItemCount: 4,
-});
+if (document.querySelector(".js-choices")) {
+    const multipleSelect = document.querySelector(".js-choices");
+    const choices = new Choices(multipleSelect, {
+        removeItemButton: true,
+        maxItemCount: 4,
+    });
+}
